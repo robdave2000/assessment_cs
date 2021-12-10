b9efa0702412bd7c33263bdfc915f8783dd21c79
Robert Davis <robdave22@yahoo.com>
2021-12-10 11:13:39 -0500

    Assessment done

--- runtime.js
+++ runtime.js
@@ -47,16 +47,107 @@ const extraLargeArray = getSizedArray(100000);
 
 // Try it with first function
 perf.start();                     // Starts timer
-doublerAppend(extraLargeArray);
+doublerAppend(tinyArray);
 let resultsAppend = perf.stop();  // Stops timer and save time results
 
 
 // Try it with second function
 perf.start();
-doublerInsert(extraLargeArray);
+doublerInsert(tinyArray);
 let resultsInsert = perf.stop();
 
 
 console.log('Results for the extraLargeArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
+
+
+
+
+function addToZero(nums)
+{
+  for (let i = 0; i < nums.length; i++)
+  {
+    for (let j = 0; j < nums.length; j++)
+    {
+      if (i === j)
+      {
+      }
+      else
+      {
+        if ((nums[i] + nums[j]) === 0)
+        {
+          console.log('True\n')
+          return true
+        }
+      }
+    }
+  }
+  console.log('False\n')
+  return false
+}
+
+addToZero([]);
+//runtime 1ops
+
+addToZero([1]);
+//runtime 2ops
+
+addToZero([1, 2, 3]);
+//runtime 10ops
+addToZero([1, 2, 3, -2]);
+//runtime 17ops
+
+function hasUniqueChars(word)
+{
+  for (let i = 0; i < word.length; i++)
+  {
+    for (let j = 0; j < word.length; j++)
+    {
+      if (i === j)
+      {
+      }
+      else
+      {
+        if (word[i] === word[j])
+        {
+          console.log('False\n')
+          return false
+        }
+      }
+    }
+  }
+  console.log('True\n')
+  return true
+}
+
+hasUniqueChars("Monday");
+//runtime 37ops
+
+hasUniqueChars("Moonday");
+//runtime 50ops
+
+function findLongestWord(words)
+{
+  let largeLength = 0
+
+  for (let i = 0; i < words.length; i++)
+  {
+    if (i === 0)
+    {
+      largeLength = words[i].length
+    }
+    else
+    {
+      if (words[i].length > largeLength)
+      {
+        largeLength = words[i].length
+      }
+    }
+  }
+
+  console.log(largeLength)
+}
+
+findLongestWord(["hi", "hello"]);
+//runtime 3ops
